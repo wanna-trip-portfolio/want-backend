b10a0bace87f32202c618a7501769021caed766c
hyukjun <hyukjuuun@gmail.com>
2022-05-01 21:45:36 +0900

    init commit

---
+++ src/main/java/com/want/want/WantApplication.java
@@ -0,0 +1,13 @@
+package com.want.want;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/want/want/WantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.want.want;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
